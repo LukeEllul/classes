2954a868c34a6210392bb7d06191cf5c667c27da
LukeEllul <ellul.luke@gmail.com>
2017-07-30 18:33:35 +0200

    added derive from

--- classes.ts
+++ classes.ts
@@ -1,7 +1,7 @@
-export const makeClass = (fn=undefined) =>
+export const makeClass = (fn = undefined) =>
     (class {
         [name: string]: any;
-        constructor(fn2){
+        constructor(fn2) {
             fn && fn.call(this);
             fn2 && fn2.call(this);
         }
@@ -12,20 +12,23 @@ export const makeClass = (fn=undefined) =>
     });
 
 export const add = (propName, prop) => cl =>
-    (cl.prototype[propName] ? console.log(`${propName} already exists`) : 
+    (cl.prototype[propName] ? console.log(`${propName} already exists`) :
         cl.prototype[propName] = prop, cl);
 
 export const addFn = fn => cl => add(fn.name, fn)(cl);
 
-export const makeObj = (cl, fn=undefined) => new cl(fn ? fn : undefined);
+export const makeObj = (cl, fn = undefined) => new cl(fn ? fn : undefined);
 
-export const child = (...classes) => (fn=undefined) => {
+export const child = (...classes) => (fn = undefined) => {
     let child = makeClass(fn);
     Object.assign(child.prototype, ...classes.map(cl => cl.prototype));
     return child;
 }
 
 export const cloneClass = cl => child(cl)(cl.getFn());
+
+export const deriveFrom = cl => (...classes) => 
+    (Object.assign(cl.prototype, ...classes.map(cl => cl.prototype)), cl);
 
 export const mix = (propName, prop) => cl =>
     add(propName, prop)(cloneClass(cl));
